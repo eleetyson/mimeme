e53b8ea231334a0f7c3227f45db1e251341d509f
Ethan <eleetyson@gmail.com>
2020-10-26 10:20:09 -0400

    build out controller actions

---
+++ app/controllers/memes_controller.rb
@@ -0,0 +1,15 @@
+class MemesController < ApplicationController
+# return information for all memes
+  def index
+    memes = Meme.all
+    if memes
+      render json: memes, include: [:comments]
+    else
+      render json: { message: 'An unexpected error occurred.' }
+    end
+  end
+
+  def show
+
+  end
+end
