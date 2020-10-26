d6b287b26c3e4cc29badec327c0963f444a32092
Ethan <eleetyson@gmail.com>
2020-10-26 09:09:39 -0400

    disable CORS for development

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,16 @@ module Mimeme
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+          :max_age => 0
+      end
+    end
+    
     config.api_only = true
   end
 end
